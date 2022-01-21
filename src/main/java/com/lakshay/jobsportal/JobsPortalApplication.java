64a05c48bb7e08d63500a9359cdf18f9c519ab12
lakshay <lakshayvohra5254@gmail.com>
2022-01-21 14:22:44 +0530

    initialize

---
+++ src/main/java/com/lakshay/jobsportal/JobsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.lakshay.jobsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsPortalApplication.class, args);
+	}
+
+}
